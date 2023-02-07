c4a18bdc519ccf199e3bfbfd6e73d45f8ae1ffbe
Oleg GLadyshev <oleg_gladysh@mail.ru>
2023-02-07 13:09:22 +0400

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+
+
+int[,] numbers = { { 1, 2, 5, 7, 19 }};
+foreach (int i in numbers)
+    Console.Write($"{i} ");
+*/
+
+int[,] numbers = { { 6, 1, 33 }};
+foreach (int i in numbers)
+    Console.Write($"{i} ");
